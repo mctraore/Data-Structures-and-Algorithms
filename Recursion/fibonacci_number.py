03878ad9510ae330cb80c7e39ade881e5fd74df0
Marie-Claire <marieclairetraore@gmail.com>
2020-07-02 20:51:34 -0400

    elegant solution fibonacci number

--- Recursion/fibonacci_number.py
+++ Recursion/fibonacci_number.py
@@ -1,11 +1,10 @@
-class Solution:
-    #elegant solution
-    def get_fib(position):
-        if position < 2:
-            return position    
-        return get_fib(position-2)+ get_fib(position-1)
+#elegant solution
+def get_fib(position):
+    if position < 2:
+        return position    
+    return get_fib(position-2)+ get_fib(position-1)
 
-    def fib_num(prev_2, prev_1, N):
+def fib_num(prev_2, prev_1, N):
     if N == 0:
         return prev_1+prev_2
     curr = prev_2+prev_1
@@ -13,8 +12,8 @@ class Solution:
     prev_1 = curr
     return fib_num(prev_2, prev_1, N-1)
 
-    def fib(self, N: int) -> int:
-        if N <2:
-            return N
-        return fib_num(0,1, N-2)
+def fib(self, N: int) -> int:
+    if N <2:
+        return N
+    return fib_num(0,1, N-2)
         
